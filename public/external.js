4060dadd9cffde08bd4f0cb2c6ae1c4f4e76df52
Logan Farmer <logan@Logans-MacBook-Pro.local>
2016-01-29 11:28:54 -0600

    added a bunch of files, some exercises, some used for exercises

---
+++ public/external.js
@@ -0,0 +1,6 @@
+var result = 4+4;
+console.log(result);
+
+console.log(1+1);
+
+console.log("hello codeup".toUpperCase);
---
+++ public/js/external.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var myAge = 25
+console.log(myAge);
+
+var result = 4+4;
+console.log(result);
+
+console.log(1+1);
+
+console.log("it's fucking lit".toUpperCase());
+
+console.log("tori is a cutie uwu".toLowerCase())
