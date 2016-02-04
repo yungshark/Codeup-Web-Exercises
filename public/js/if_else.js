80e13fa508ea538ef3085778e1d100be12de7cb8
Logan Farmer <logan@Logans-MacBook-Pro.local>
2016-02-04 11:44:38 -0600

    committing old files and adding one regarding split and joining arrays

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -3,33 +3,29 @@
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 var color = colors[Math.floor(Math.random()*colors.length)];
 
-var favorite = 'indigo'; // TODO: change this to your favorite color from the list
+switch (color) {
+	case "red":
+		console.log('this color is red');
+		break;
+	case "orange":
+		console.log('this color is orange');
+		break;
+	case "yellow":
+		console.log('this color is yellow');
+		break;
+	case "green":
+		console.log('this color is green');
+		break;
+	case "blue":
+		console.log('this color is blue');
+		break;
+	default:
+		
 
-// TODO: Create a block of if/else statements to check for every color except indigo and violet.
-if (color == 'red') {
-	console.log('its red and so is my bloody nose');
-} else if (color == 'orange'){
-	console.log('its orange and so is the sun');
-} else if (color == 'yellow'){
-	console.log('its yellow and so is a duck');
-} else if (color == 'green'){
-	console.log('its green and so is MONEY');
-} else if (color == 'blue'){
-	console.log('its blue and so is the sea');
-} else {
-	console.log('i dont know anything by that color other than like flowers and shit');
+    // todo: create a case statement that will handle every color except indigo and violet
+    // todo: when a color is encountered log a message that tells the color, and an object of that color
+    //       example: Blue is the color of the sky.
+
+    // todo: create a default case that will catch indigo and violet
+    // todo: for the default case, log: I do not know anything by that color.
 }
-
-console.log((favorite == color)? color + "  is my favourite color!" : color + " is not my favourite color...");
-
-
-
-// TODO: When a color is encountered log a message that tells the color, and an object of that color.
-//       Example: Blue is the color of the sky.
-
-// TODO: Have a final else that will catch indigo and violet.
-
-// TODO: In the else, log: I do not know anything by that color.
-
-// TODO: Using the ternary operator, conditionally log a statement that
-//       says whether the random color matches your favorite color.
